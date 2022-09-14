43be16bc9b2c03a2ef5b9500f276f310d19617b3
Jaecobd1 <jaecob.d1@gmail.com>
2022-09-14 19:56:47 -0400

    added videography section

--- javascript/script.js
+++ javascript/script.js
@@ -2,9 +2,9 @@ const timeline = gsap.timeline()
 
 timeline
     .from('.hero', { duration: 3.2, opacity: 0, ease: 'easy' })
-    .from('nav', {duration: 1.3, opacity: 0})
-    .from('.nav-link', {opacity: 0, stagger: 0.5})
-    .from('.first', { duration: 3, opacity: 0, x: '-100%', })
-    .from('.last', { duration: 3, opacity: 0, y: '100%', })
+    .from('nav', {duration: 0.75, opacity: 0})
+    .from('.nav-link', {opacity: 0, stagger: 0.25})
+    .from('.first', { duration: 2, opacity: 0, x: '-100%', })
+    .from('.last', { duration: 2, opacity: 0, y: '100%', })
     .from('.content', {duration: 2, opacity: 0})
     .fromTo('.button-container', {duration: 2, opacity: 0})
